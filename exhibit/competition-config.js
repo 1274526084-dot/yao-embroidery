9fc9ce538143fba51edbd5197225928ba615ced0
1274526084-dot <1274526084@qq.com>
2026-09-18 01:16:40 +0800

    Enable desktop archives to sync with mobile museum

--- exhibit/competition-config.js
+++ exhibit/competition-config.js
@@ -1,5 +1,5 @@
 window.__YAO_COMPETITION_CONFIG__ = window.__YAO_COMPETITION_CONFIG__ || {
   aiProxyUrl: '',
-  archiveApiUrl: '',
+  archiveApiUrl: 'https://yaoembroidery-d8gielpuud108441b-1431364187.ap-shanghai.app.tcloudbase.com/archive-sync',
   requestTimeoutMs: 6500,
 };
